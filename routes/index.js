a9cfeca1b76ddc094922154cda204ede7dab915c
wushaobin <wushaobin@globalegrow.com>
2018-01-25 16:42:48 +0800

    支持es6写法

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // 引入mongoose
 let mongoose = require('mongoose')
-// 用于异步回调
+    // 用于异步回调
 mongoose.Promise = require('bluebird')
 global.db = mongoose.connect('mongodb://localhost:27017/lvyou', { useMongoClient: true })
 global.db.on('error', console.error.bind(console, '连接错误:'))
-global.db.once('open', function () {
+global.db.once('open', function() {
     console.log('Mongodb running')
 })
 
@@ -17,33 +17,33 @@ const Jingdian = require('../reptile/models/jingdian')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// 获取旅游城市信息
-	Citys.find().limit(5).sort("{ createTime: -1 }").exec(function (err, message) {
-	    if (err) {
-	        console.log(err)
-	    } else {
-			console.log(JSON.parse(JSON.stringify(message)))
-			// console.log(message[0].business)
-	    }
-	})
+    // 获取旅游城市信息
+    Citys.find().limit(5).sort("{ createTime: -1 }").exec(function(err, message) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(JSON.parse(JSON.stringify(message)))
+                // console.log(message[0].business)
+        }
+    })
 
-	Fengjing.find().limit(1).sort("{ createTime: -1 }").exec(function (err, message) {
-	    if (err) {
-	        console.log(err)
-	    } else {
-			console.log(JSON.parse(JSON.stringify(message)))
-	    }
-	})
+    Fengjing.find().limit(1).sort("{ createTime: -1 }").exec(function(err, message) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(JSON.parse(JSON.stringify(message)))
+        }
+    })
 
-	Jingdian.find().limit(1).exec(function (err, message) {
-	    if (err) {
-	        console.log(err)
-	    } else {
-	    	console.log(JSON.parse(JSON.stringify(message)))
-	    }
-	})
+    Jingdian.find().limit(1).exec(function(err, message) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(JSON.parse(JSON.stringify(message)))
+        }
+    })
 
-   res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
