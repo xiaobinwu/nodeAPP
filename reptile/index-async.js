358cd7d5abc3710c5b30a6f14943bb6461656fb7
xiaobinwu <xiaobin_wu@yahoo.com>
2018-01-26 00:44:19 +0800

    生成日志

--- reptile/index-async.js
+++ reptile/index-async.js
@@ -12,19 +12,21 @@ import bluebird from 'bluebird'
 
 const access = util.promisify(fs.access)
 const writeFile = util.promisify(fs.writeFile)
+const mkdir = util.promisify(fs.mkdir)
 
+console.log(access)
 
 // 引入mongoose
-import mongoose from 'mongoose'
-let Promise;
-//开启日志
-mongoose.set('debug', true);
-// promise化
-mongoose.Promise = Promise = bluebird
-global.db = mongoose.connect('mongodb://localhost:27017/lvyou', {
-    useMongoClient: true
-})
-global.db.on('error', console.error.bind(console, '连接错误:'))
+// import mongoose from 'mongoose'
+// let Promise;
+// //开启日志
+// mongoose.set('debug', true);
+// // promise化
+// mongoose.Promise = Promise = bluebird
+// global.db = mongoose.connect('mongodb://localhost:27017/lvyou', {
+//     useMongoClient: true
+// })
+// global.db.on('error', console.error.bind(console, '连接错误:'))
 
 
 import Fengjing from './models/fengjing'
@@ -38,23 +40,17 @@ const httpRequest = (options) => {
         resolveWithFullResponse: true // Get the full response instead of just the body
     }
     return new Promise((resolve, reject) => {
-        rp(options).then((response) => {
+        rp(options).then(async (response) => {
 
+            let dirUrl = './reptile/log'
+            const err = await access(dirUrl, fs.constants.R_OK | fs.constants.W_OK)
+            if(err) {
+                await mkdir(dirUrl)
+                await writeFile(`${dirUrl}/${new Date().toUTCString()}.txt`, JSON.stringify(response))
+            } else {
+                await writeFile(`${dirUrl}/${new Date().toUTCString()}.txt`, JSON.stringify(response))
+            }
 
-            const err = awiat access('./reptile/log', fs.constants.R_OK | fs.constants.W_OK)
-
-            console.log(err)
-
-            // fs.exists('./log', (exists) => {
-            //     fs.mkdir('./log', 0777, (err) => {
-            //         if (err) throw err;
-            //         fs.writeFileSync(`./log/a.txt`, JSON.stringify(response), (err) => {
-            //             if (err) { throw err }
-            //         })
-            //     });
-            // })
-
-            console.log(response);
 
         }).catch((err) => {
             throw err
